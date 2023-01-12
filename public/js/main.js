1c4f59c7766c3797a86091f12e328180b8972416
VitorLucas44 <vitoriosolucas@hotmail.com>
2023-01-12 14:50:10 +0100

    exo1

---
+++ public/js/main.js
@@ -0,0 +1,20 @@
+// 1
+// let h1Elements = document.getElementsByTagName("h1");
+// console.log(h1Elements);
+
+// 2
+// let pElements = document.getElementsByTagName("p");
+// let h3un = document.getElementsByTagName("h3");
+// let h3Deux = document.getElementsByTagName("h3");
+// let h3Trois = document.getElementsByTagName("h3");
+// console.log(h3un[0])
+// console.log(h3Deux[1])
+// console.log(h3Trois[2])
+
+// 3
+// var pElements = document.getElementsByTagName("p");
+// console.log(pElements[1].textContent);
+
+// 4
+// var liElements = document.getElementsByTagName("li");
+// console.log(liElements[2]);
